3158e98ef5d21193a776d90254a9134fd5d84673
Erlend Hamnaberg <erlend@hamnaberg.net>
2017-01-09 14:58:50 +0100

    Fix to compile on Scala 2.12

--- src/main/scala/d2/Sync.scala
+++ src/main/scala/d2/Sync.scala
@@ -3,17 +3,17 @@ package d2
 import unfiltered.request.HttpRequest
 import unfiltered.response.ResponseFunction
 
-object Sync extends Sync[Any](PartialFunction.empty)
+object Sync extends Sync[Any](PartialFunction.empty) {
+  type Id[+A] = A
+}
 
 case class Sync[B](errorHandler:PartialFunction[Throwable, ResponseFunction[B]]) {
-  type Id[+A] = A
-
-  def id[A, BB <: B](pf:PartialFunction[HttpRequest[A], Directive[A, Id, ResponseFunction[BB], ResponseFunction[BB]]]): unfiltered.Cycle.Intent[A, BB] = {
+  def id[A, BB <: B](pf:PartialFunction[HttpRequest[A], Directive[A, Sync.Id, ResponseFunction[BB], ResponseFunction[BB]]]): unfiltered.Cycle.Intent[A, BB] = {
     case req if pf.isDefinedAt(req)  => Result.merge(pf.apply(req).run(req))
   }
 
   case class Mapping[T, X](from: HttpRequest[T] => X) {
-    def apply[TT <: T, BB <: B](intent: PartialFunction[X, Directive[TT, Id, ResponseFunction[BB], ResponseFunction[BB]]]) = id[TT, BB] {
+    def apply[TT <: T, BB <: B](intent: PartialFunction[X, Directive[TT, Sync.Id, ResponseFunction[BB], ResponseFunction[BB]]]) = id[TT, BB] {
       case req if intent.isDefinedAt(from(req)) => intent(from(req))
     }
   }
